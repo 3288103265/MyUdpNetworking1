5b3eb7d58cb5309940cc8c4b32aea63728505b6c
3288103265 <3288103265@qq.com>
2019-04-09 16:36:18 +0800

    Add project files.

---
+++ UdpSender/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace UdpSenderPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient(0);
+
+            udpClient.Connect("", 11000);
+            byte[] sendBytes = Encoding.ASCII.GetBytes("Is anybody there?");
+            udpClient.Send(sendBytes, sendBytes.Length);
+
+            udpClient.Close();
+        }
+    }
+}
---
+++ UdpServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpServer = new UdpClient(11000);
+
+            IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            byte[] receiveBytes = udpServer.Receive(ref remoteIpEndPoint);
+            string revMsg = Encoding.ASCII.GetString(receiveBytes);
+            Console.WriteLine($"Received: {revMsg}");
+
+            udpServer.Close();
+
+        }
+    }
+}
